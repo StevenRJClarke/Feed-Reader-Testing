dc542126459aaab5ed7a2ea794cceb62ea30dfad
StevenRJClarke1985 <srjc3@cantab.net>
2018-12-02 10:50:35 +0000

    Created test suite for "The menu"

--- test/spec/feedreader.js
+++ test/spec/feedreader.js
@@ -21,7 +21,7 @@
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-         it('are defined', function() {
+        it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -65,10 +65,11 @@
                 expect(name.length).not.toBe(0);
             })
         });
-     });
+    });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The Menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -82,6 +83,7 @@
           * clicked and does it hide when clicked again.
           */
 
+    });
           /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
