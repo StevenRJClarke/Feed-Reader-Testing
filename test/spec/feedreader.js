ba99a9f3273e6bda63bd48d2b7d1949319afd42e
StevenRJClarke1985 <srjc3@cantab.net>
2018-12-03 04:13:39 +0000

    Removed capital letter from menu in describe

--- test/spec/feedreader.js
+++ test/spec/feedreader.js
@@ -69,7 +69,7 @@
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe('The Menu', function() {
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
